777e42cf3519e94d607b89753c859162c26f5a9e
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-03-26 19:59:13 +0200

    Add file

---
+++ reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
+
+const reducer = combineReducers({
+    comments,
+    users,
+
+});
+
+export default reducer;
